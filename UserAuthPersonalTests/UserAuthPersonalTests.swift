4eab9adfe73c565c07b29fbbed5ce6c62ccc8395
Shubroto <shuvotech25@gmail.com>
2019-09-05 11:13:41 -0500

    Initial Commit

---
+++ UserAuthPersonalTests/UserAuthPersonalTests.swift
@@ -0,0 +1,34 @@
+//
+//  UserAuthPersonalTests.swift
+//  UserAuthPersonalTests
+//
+//  Created by IMCS2 on 9/5/19.
+//  Copyright © 2019 Tanishk. All rights reserved.
+//
+
+import XCTest
+@testable import UserAuthPersonal
+
+class UserAuthPersonalTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
